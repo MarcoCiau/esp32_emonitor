ef83c2d0f9738f4d2ade6bc5f49cfeebafc81bea
MarcoCiau <marcoantoniociau@hotmail.com>
2023-08-17 11:12:20 -0600

    running current sensors

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 #include <ADC_74HC4066.h>
-#include <hwConfig.h>
 #include <CurrentSensors.h>
 void setup()
 {
@@ -11,14 +10,5 @@ void setup()
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
   currentSensors.loop();
-    // Serial.println("New ADC Read");
-  // for (uint8_t i = 0; i < 3; i++)
-  // {
-  //   Serial.printf("ADC%d: %d,", i + 1, adc.read(i));
-  //   delay(20);
-  // }
-  // Serial.println();
-  // delay(250);
 }
