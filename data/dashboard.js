cbeb3ef2aa7baf9058ad2d2432657d547cc1cf0b
MarcoCiau <marcoantoniociau@hotmail.com>
2023-08-26 15:08:03 -0600

    fix dashboard behaviour by only refresh current sensors values

--- data/dashboard.js
+++ data/dashboard.js
@@ -1,4 +1,49 @@
-document.addEventListener('DOMContentLoaded', () => { 
+let tableIsBuilt = false;
+let fetchCalibrationFirstTime = true;
+function buildSensorTable(calibrationValues, currentValues) {
+  if (!tableIsBuilt) {
+    const sensorTableBody = document.getElementById('sensorTableBody');
+    for (let channel = 0; channel < 16; channel++) {
+      const newRow = document.createElement('tr');
+
+      const sensorIdCell = document.createElement('td');
+      sensorIdCell.textContent = `Channel ${channel}`;
+
+      const calibrationCell = document.createElement('td');
+      const calibrationInput = document.createElement('input');
+      calibrationInput.type = 'number';
+      calibrationInput.step = '0.01';
+      calibrationInput.value = calibrationValues[channel];
+      calibrationInput.className = 'calibration-input';
+      calibrationCell.appendChild(calibrationInput);
+
+      const sensorValueCell = document.createElement('td');
+      sensorValueCell.className = 'sensor-value';
+      sensorValueCell.textContent = `${currentValues[channel]} A`;
+
+      const calibrateCell = document.createElement('td');
+      const calibrateButton = document.createElement('button');
+      calibrateButton.textContent = 'Calibrate';
+      calibrateButton.onclick = () => calibrate(channel, calibrationInput);
+      calibrateCell.appendChild(calibrateButton);
+
+      newRow.appendChild(sensorIdCell);
+      newRow.appendChild(calibrationCell);
+      newRow.appendChild(sensorValueCell);
+      newRow.appendChild(calibrateCell);
+      
+      sensorTableBody.appendChild(newRow);
+    }
+
+    tableIsBuilt = true;
+  }
+  else
+  {
+    updateTable(currentValues);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
     // Function to fetch and update calibration and current values
     function updateValues() {
       // Fetch calibration and current values
@@ -8,7 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
           fetch('/current')
             .then(response => response.json())
             .then(currentValues => {
-              updateTable(calibrationValues, currentValues);
+              buildSensorTable(calibrationValues, currentValues);
             });
         });
     }
@@ -17,39 +62,13 @@ document.addEventListener('DOMContentLoaded', () => {
     setInterval(updateValues, 5000);
   });
   
-  function updateTable(calibrationValues, currentValues) {
+  function updateTable( currentValues) {
+    if (!tableIsBuilt) return;
     const sensorTableBody = document.getElementById('sensorTableBody');
-    sensorTableBody.innerHTML = '';
-  
+    const sensorValueLabels = sensorTableBody.getElementsByClassName('sensor-value');
     for (let channel = 0; channel < 16; channel++) {
-      const newRow = document.createElement('tr');
-      
-      const sensorIdCell = document.createElement('td');
-      sensorIdCell.textContent = `Channel ${channel}`;
-      
-      const calibrationCell = document.createElement('td');
-      const calibrationInput = document.createElement('input');
-      calibrationInput.type = 'number';
-      calibrationInput.step = '0.01';
-      calibrationInput.value = calibrationValues[channel];
-      calibrationInput.className = 'calibration-input';
-      calibrationCell.appendChild(calibrationInput);
-      
-      const sensorValueCell = document.createElement('td');
-      sensorValueCell.className = 'sensor-value';
-      sensorValueCell.textContent = currentValues[channel] + ' A';
-      
-      const calibrateCell = document.createElement('td');
-      const calibrateButton = document.createElement('button');
-      calibrateButton.textContent = 'Calibrate';
-      calibrateButton.onclick = () => calibrate(channel, calibrationInput);
-      calibrateCell.appendChild(calibrateButton);
-  
-      newRow.appendChild(sensorIdCell);
-      newRow.appendChild(calibrationCell);
-      newRow.appendChild(sensorValueCell);
-      newRow.appendChild(calibrateCell);
-      sensorTableBody.appendChild(newRow);
+      // Update the sensor value label for the channel
+      sensorValueLabels[channel].textContent = currentValues[channel] + ' A';
     }
   }
   
